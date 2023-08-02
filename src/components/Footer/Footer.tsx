0b79b0ea282e49f441e98d314c88bbbec52663f8
OksanaZhuravel <opuseva@mail.ru>
2023-08-02 19:08:57 +0400

    change Footer Components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import NavBar from "../NavBar/NavBar";
+import Social from "../Social/Social";
 
 
 const Footer = () => {
@@ -20,6 +21,19 @@ const Footer = () => {
             <p className="footer__text">&copy;&nbsp;Все права защищены</p>
             <Link to="/" className="footer__link"><span className="footer__text">Политика конфиденциальности</span></Link>
             <Link to="/" className="footer__link"><span className="footer__text">Публичная оферта</span></Link>
+          </div>
+          <ul className="footer__list">
+            <li className="footer__item"><Link className="footer__link" to='/'><span className="footer__text">Пальто</span></Link></li>
+            <li className="footer__item"><Link className="footer__link" to='/'><span className="footer__text">Свитшоты</span></Link></li>
+            <li className="footer__item"><Link className="footer__link" to='/'><span className="footer__text">Кардиганы</span></Link></li>
+            <li className="footer__item"><Link className="footer__link" to='/'><span className="footer__text">Толстовки</span></Link></li>
+
+          </ul>
+          <div className="footer__inner">
+            <Social />
+            <div className="footer__pay">
+              <img src="/assets/img/icon/visa-mastercard.png" alt="visa-mastercard" />
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/Social/Social.tsx
@@ -0,0 +1,20 @@
+import Facebook from '../Icon/Facebook';
+import Instagram from '../Icon/Instagram';
+import Twitter from '../Icon/Twitter';
+
+const Social = () => {
+	return (
+		<div className='footer__social social'>
+			<a href='https://www.instagram.com/' className='social__icon' target='_blank' title='Instagram'>
+				<Instagram />
+			</a>
+			<a href='https://www.facebook.com/' className='social__icon' target='_blank' title='Facebook'>
+				<Facebook />
+			</a>
+			<a href='https://www.twitter.com/' className='social__icon' target='_blank' title='Twitter'>
+				<Twitter />
+			</a>
+		</div>
+	);
+};
+export default Social;
